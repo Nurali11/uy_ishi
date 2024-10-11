e88ab777a5b759f52c4031c3df22c24e1fb43753
Nurali11 <boltaboyevnurali55@gmal.com>
2024-10-11 16:26:23 +0500

    info.txt

--- uy_ishi.py
+++ uy_ishi.py
@@ -1,4 +1,8 @@
-print('salom')
-print('Nimadur')
-print('Qaleslar')
-print('Kimdur')
+class Book:
+    def __init__(self,id,name,author,count,narx):
+        self.id = id
+        self.name = name
+        self.avtor = author
+        self.count = count
+        self.price = narx
+    
